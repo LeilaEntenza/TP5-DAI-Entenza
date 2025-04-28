450fa6eb68f5efff055bf575004b196103b4ca1f
LEILA ALUMNE ENTENZA CHIDERSKI <48385438@ort>
2025-04-28 09:34:26 -0300

    done!!

--- src/app.js
+++ src/app.js
@@ -15,6 +15,6 @@ app.get('/fecha-completa', (req, res)=>{
 })
 
 app.use((req, res, next) => {
-    res.status(404).send(":(");
+    res.status(404).send('<img src="https://http.cat/404>');
 })
 export default app;
